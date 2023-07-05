46856ca97b4c3a27c55cfab5cc14bc8c1ccb889a
Natalia-Havashi <natali.gavashi@gmail.com>
2023-07-05 22:35:13 +0300

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import { Button } from "./Button/Button";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { ImageGalleryItem } from "./ImageGalleryItem/ImageGalleryItem";
+import { Loader } from "./Loader/Loader";
+import { Modal } from "./Modal/Modal";
+import { Searchbar } from "./Searchbar/Searchbar";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +17,12 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Searchbar onSubmit={<></>}/>
+      <ImageGallery />
+      <ImageGalleryItem />
+      <Button />
+      <Loader />
+     <Modal />
     </div>
   );
 };
